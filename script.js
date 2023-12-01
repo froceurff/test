5fbe318d93e124351c07e2eb3d4c637b26bfaf26
froceurff <152653521+froceurff@users.noreply.github.com>
2023-12-01 22:38:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const generateButton = document.getElementById('generate');
     const passwordList = document.getElementById('password-list');
     const notification = document.getElementById('notification');
+    const downloadButton = document.getElementById('downloadButton');
 
     generateButton.addEventListener('click', function () {
         const passwordLength = document.getElementById('length').value;
@@ -17,6 +18,26 @@ document.addEventListener('DOMContentLoaded', function () {
         displayPassword(newPassword, passwordTitle);
         showNotification('Nouveau mot de passe généré et sauvegardé!');
     });
+
+    downloadButton.addEventListener('click', downloadPasswords);
+
+    function downloadPasswords() {
+        const passwords = [];
+        const passwordItems = document.querySelectorAll('.password-list li');
+        
+        passwordItems.forEach(item => {
+            const title = item.querySelector('strong').textContent;
+            const password = item.textContent.split(":")[1].trim();
+            passwords.push(`${title} ${password}`);
+        });
+
+        const content = passwords.join('\n');
+        const blob = new Blob([content], { type: 'text/plain' });
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'passwords.txt';
+        a.click();
+    }
 
     function generatePassword(length) {
         const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -35,18 +56,12 @@ document.addEventListener('DOMContentLoaded', function () {
         passwordItem.innerHTML = `<strong>${title}:</strong> ( ${password} )`;
         savedPasswordsList.appendChild(passwordItem);
 
-        // Enregistrer le titre et le mot de passe dans le localStorage
         savePassword({ title, password });
     }
 
     function savePassword(passwordObject) {
-        // Récupérer les mots de passe sauvegardés depuis le localStorage
         const savedPasswords = JSON.parse(localStorage.getItem('savedPasswords')) || [];
-
-        // Ajouter le nouvel objet {title, password} au tableau
         savedPasswords.push(passwordObject);
-
-        // Enregistrer le tableau mis à jour dans le localStorage
         localStorage.setItem('savedPasswords', JSON.stringify(savedPasswords));
     }
 
@@ -54,11 +69,11 @@ document.addEventListener('DOMContentLoaded', function () {
         notification.textContent = message;
         notification.style.display = 'block';
 
-        // Cacher la notification après quelques secondes (ajustez la durée si nécessaire)
         setTimeout(function () {
             notification.style.display = 'none';
         }, 3000);
     }
 
-
+    const savedPasswords = JSON.parse(localStorage.getItem('savedPasswords')) || [];
+    displayPasswords(savedPasswords);
 });
